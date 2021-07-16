6865490bcab66bf8cfe15e5ed69182ef70ed0c72
orgrinberg <grinberg.or@gmail.com>
2021-07-16 19:10:12 +0300

    new header file

---
+++ project1/include/udp.h
@@ -0,0 +1,11 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define PORT 4080      // The port number.
+#define MAXLINE 1024   // The maximum lengh to the string.
