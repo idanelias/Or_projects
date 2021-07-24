2a9415f0c5119b6071e8c77ec855b1cde48197af
orgrinberg <grinberg.or@gmail.com>
2021-07-24 20:40:03 +0300

    remove include and define from src files

--- chat/src/chatclient.c
+++ chat/src/chatclient.c
@@ -1,16 +1,4 @@
-//#include "../2/chat.h"
-
-#include <netinet/in.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <signal.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <pthread.h>
+#include "../include/chat.h"
 
 #define LEN 2048
 
--- chat/src/chatserver.c
+++ chat/src/chatserver.c
@@ -1,20 +1,4 @@
-//#include "../2/chat.h"
-
-#include <netinet/in.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <signal.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <pthread.h>
-
-#define MAX_CLIENTS 100
-#define LEN 2048
-
+#include "../include/chat.h"
 
 static _Atomic unsigned int cli_count = 0;
 static int uid = 10;
